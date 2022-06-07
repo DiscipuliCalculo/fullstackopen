7110c7b3508d6d2587a4cbc42bfb72de4e410920
Vitam Discipuli <discipulicalculo@gmail.com>
2022-06-06 20:21:21 -0700

    completed exercise 1.5

--- part1/src/App.js
+++ part1/src/App.js
@@ -3,27 +3,29 @@ import Header from "./components/Total";
 import Total from "./components/Total";
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts = {[parts[0].name, parts[1].name, parts[2].name]} exercises = {[parts[0].exercises, parts[1].exercises, parts[2].exercises]}/>
-      <Total exercise_total={parts[0].exercises + parts[1].exercises + parts[2].exercises}/>
+      <Header course={course.name} />
+      <Content parts = {[course.parts[0].name, course.parts[1].name, course.parts[2].name]} exercises = {[course.parts[0].exercises, course.parts[1].exercises, course.parts[2].exercises]}/>
+      <Total exercise_total={course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises}/>
     </div>
   )
 }
