feccda02745bc05b5dae7514c0e56cc2d247c729
Vitam Discipuli <discipulicalculo@gmail.com>
2022-06-06 22:38:24 -0700

    completed exercise 1.11

--- part1/exercises-1.6-1.14/src/App.js
+++ part1/exercises-1.6-1.14/src/App.js
@@ -12,13 +12,15 @@ const Statistics = (props) => {
   }
   return (
     <div>
-      <h3>statistics</h3>
-      <StatisticsLine text="good" value={props.good} />
-      <StatisticsLine text="neutral" value={props.neutral} />
-      <StatisticsLine text="bad" value={props.bad} />
-      <StatisticsLine text="all" value={props.all} />
-      <StatisticsLine text="average" value={props.average} />
-      <StatisticsLine text="positive" value={props.positive} />
+      <table>
+        <h3>statistics</h3>
+        <StatisticsLine text="good" value={props.good} />
+        <StatisticsLine text="neutral" value={props.neutral} />
+        <StatisticsLine text="bad" value={props.bad} />
+        <StatisticsLine text="all" value={props.all} />
+        <StatisticsLine text="average" value={props.average} />
+        <StatisticsLine text="positive" value={props.positive} />
+      </table>
     </div>
   )
 }
