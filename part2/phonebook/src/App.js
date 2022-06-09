ed6b3be2187cd4fb327acea070dbb114cdf4ef25
Vitam Discipuli <discipulicalculo@gmail.com>
2022-06-09 11:36:00 -0700

    completed exercise 2.8

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,13 +2,20 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas',
+      number: '213-321-6435'
+     }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
     /* The handleChange() function to set a new state for input */
-    const handleChange = (e) => {
+    const handleNameChange = (e) => {
       setNewName(e.target.value);
+    }
+
+    const handleNumberChange = (e) => {
+      setNewNumber(e.target.value);
     }
 
     const handleSubmit = (e) => {
@@ -17,7 +24,7 @@ const App = () => {
       if (foundItem) {
         return alert(`${newName} already exists`)
       }
-      setPersons(persons.concat({name: newName}))
+      setPersons(persons.concat({name: newName, number: newNumber}))
     }
 
   return (
@@ -25,7 +32,8 @@ const App = () => {
       <h2>Phonebook</h2>
       <form onSubmit={handleSubmit}>
         <div>
-          name: <input value={newName} onChange={handleChange}/>
+          name: <input value={newName} onChange={handleNameChange}/>
+          number: <input value={newNumber} onChange={handleNumberChange}/>
         </div>
         <div>
           <button type="submit">add</button>
@@ -33,7 +41,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-      {persons.map(person => <li>{person.name}</li> )}
+      {persons.map(person => <li>{person.name} {person.number}</li> )}
       </ul>
     </div>
   )
