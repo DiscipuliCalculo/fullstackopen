d13b1e7b01921377c20cab507bf3ca8158a26325
Vitam Discipuli <discipulicalculo@gmail.com>
2022-06-08 22:12:32 -0700

    completed 2.2 and 2.3

--- part2/exercises-2.1-2.5/src/components/Content.js
+++ part2/exercises-2.1-2.5/src/components/Content.js
@@ -1,9 +1,14 @@
 import Part from "./Part"
 
 const Content = ({parts}) => {
+  const totalExercises = parts.reduce((total, part) => {
+    return total + part.exercises
+  }, 0)
+
   return (
     <div>
       {parts.map(part => <Part name={part.name} exercises={part.exercises}/>)}
+      <h3>total of {totalExercises} exercises</h3>
     </div>
   )
 }
