fce8ce12cc86b3cb1a4a01ba41fe18019f6706bc
Vitam Discipuli <discipulicalculo@gmail.com>
2022-06-08 22:26:46 -0700

    completed 2.5

--- part2/exercises-2.1-2.5/src/components/Course.js
+++ part2/exercises-2.1-2.5/src/components/Course.js
@@ -2,11 +2,58 @@ import Header from "./Header"
 import Content from "./Content"
 
 const Course = ({course}) => {
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
   return ( 
   <div>
-    <Header name={course.name} />
-    <Content parts={course.parts} />
+    {courses.map(course => 
+    <div>
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+    </div>)}
   </div>
   )
   
