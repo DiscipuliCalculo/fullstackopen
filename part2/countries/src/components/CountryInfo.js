3b34fd77ca8bec590a2a15382ffcafdc5e1995bc
Vitam Discipuli <discipulicalculo@gmail.com>
2022-06-12 10:23:23 -0700

    completed exercise 2.13

--- part2/countries/src/components/CountryInfo.js
+++ part2/countries/src/components/CountryInfo.js
@@ -1,5 +1,16 @@
+import { useState, useEffect } from "react"
 
 const CountryInfo = ({countries}) => {
+  const [selection, setSelection] = useState()
+
+  function setInfo(id) {
+    setSelection(countries[id])
+  }
+
+  function showInfo(id) {
+    setInfo(id)
+
+  }
 
   if (countries.length ===  1) {
     return (
@@ -9,9 +20,25 @@ const CountryInfo = ({countries}) => {
         <p>area: {countries[0].area}</p>
         <h3>languages:</h3>
         <ul>
-          {Object.entries(countries[0].languages).map((language) => <li>{Object.values(language)[1]}</li>)}
+          {Object.entries(countries[0].languages).map((language, index) => <li key={index}>{Object.values(language)[1]}</li>)}
         </ul>
         <img src={countries[0].flags.png} alt="flag of the country"/>
+      </div>
+    )
+  }
+
+  else if (selection !== undefined) {
+    return (
+      <div>
+        <button onClick={() => setSelection()}>Back</button>
+        <h2>{selection.name.common}</h2>
+        <p>capital: {selection.capital[0]}</p>
+        <p>area: {selection.area}</p>
+        <h3>languages:</h3>
+        <ul>
+          {Object.entries(selection.languages).map((language, index) => <li key={index}>{Object.values(language)[1]}</li>)}
+        </ul>
+        <img src={selection.flags.png} alt="flag of the country"/>
       </div>
     )
   }
@@ -19,7 +46,7 @@ const CountryInfo = ({countries}) => {
   else if (countries.length > 1 && countries.length < 10) {
     return (
       <ul>
-        {countries.map(country => <li>{country.name.common}</li> )}
+        {countries.map((country, index) => <div key={index} listid={index}>{country.name.common} <button onClick={() => showInfo(index)}>Show</button></div> )}
       </ul>
     )
   }
